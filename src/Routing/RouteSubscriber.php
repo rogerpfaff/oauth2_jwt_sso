96f1f60a6a29ea740b7197ee4be1632da1b5ecad
alan_blake <alan_blake@3050599.no-reply.drupal.org>
2019-02-19 16:47:40 +0800

    Issue #3034020 by alan_blake: Fix redirect auth server logout url directly without destroy session

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -11,6 +11,7 @@ class RouteSubscriber extends RouteSubscriberBase{
   protected function alterRoutes(RouteCollection $collection) {
     if ($route =  $collection->get('user.logout')) {
       $route->setDefault('_controller', '\Drupal\oauth2_jwt_sso\Controller\OAuth2JwtSSOController::logout');
+      $route->setOptions(['no_cache'=>TRUE]);
     }
   }
 
